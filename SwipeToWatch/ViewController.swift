91ceca48bbe9af7760a2f712575bc450bc6939c0
Bagas Ilham Rabbani <bagas.rabbani@nostratech.com>
2025-01-25 10:13:47 +0700

    Initial Commit

---
+++ SwipeToWatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeToWatch
+//
+//  Created by Bagas Ilham on 25/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
